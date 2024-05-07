2b1d5bd3cc8bfc1ddbafc3d653503a9699042328
Anderson Silva <nutriandersonsilva@gmail.com>
2024-05-07 13:18:10 -0300

    atividade 3

--- src/components/PokemonDetails.js
+++ src/components/PokemonDetails.js
@@ -1,20 +1,34 @@
-import React, { Component } from 'react';
 import propTypes from 'prop-types';
+import React, { Component } from 'react';
+import Pokemon from './Pokemon';
 
-export default class PokemonDetails extends Component {
+export default class PokemonExtra extends Component {
   render() {
     const { pokemonList, match: { params: { id } } } = this.props;
     const pokemon = pokemonList.find((poke) => poke.id === Number(id));
     return (
-      <div>
+      <section className="pokemon-details">
         <h1>
           {`${pokemon.name} details`}
         </h1>
-      </div>
+        <Pokemon pokemon={ pokemon } />
+        <h2>Sumário:</h2>
+        <p>{pokemon.summary}</p>
+        <h2>Hábitat:</h2>
+        <section className="pokemon-habitat">
+          { pokemon.foundAt.map((location) => (
+            <section key={ location.location }>
+              <span>{ location.location }</span>
+              <img src={ location.map } alt="mapa do pokemon" />
+            </section>
+          )) }
+        </section>
+      </section>
     );
   }
 }
-PokemonDetails.propTypes = {
+
+PokemonExtra.propTypes = {
   pokemonList: propTypes.arrayOf(propTypes.shape).isRequired,
   match: propTypes.shape({
     params: propTypes.shape({
